196cfc0bf181b7a129a3eec0047d983353f87e3d
Thanamond <122435863+Thanamond@users.noreply.github.com>
2023-05-14 21:50:33 +0700

    Add files via upload

---
+++ score(2 dimension array).cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float [,] score = new float[2,5];
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Console.Write("Score : ");
+                    score[row, col] = float.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int row = 0; row < 2; row++)
+            {
+                float sum = 0;
+                for (int col = 0; col < 5; col++)
+                {
+                    sum += score[row, col];
+                }
+                Console.WriteLine(sum);
+            }
+
+        }   
+    }
+}
